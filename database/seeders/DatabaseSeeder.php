9a66fa07f18d2cb028b8e8dedf70c01fd1d06469
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-09-08 20:51:52 +0800

    create role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,14 +5,19 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
+
     public function run(): void
     {
+
+        Role::create(['name' => 'company']);
+        Role::create(['name' => 'employee']);
 
         $this->call([
             CompanySeeder::class,
