b24f9acc430310f2b1c390d6eaad8950c829fbc0
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-09-08 16:55:52 +0800

    avatar admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,8 @@
 <x-app-layout>
-
     <div class="px-4 text-gray-900">
         <a href={{ route('job-post.create') }}>
             <x-primary-button class="mb-4">{{ __('Post Job') }}</x-primary-button>
         </a>
         <livewire:job-post-table/>
     </div>
-
-
-
 </x-app-layout>
