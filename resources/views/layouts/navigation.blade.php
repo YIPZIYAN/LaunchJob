126c6200e683253dcf352078ccc7c245a32617b5
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-09-09 23:59:57 +0800

    responsive nav link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -71,22 +71,29 @@
                 <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                     <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                         <li>
-                            <a href="{{route('home')}}"
-                               class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                               aria-current="page">Home</a>
+                            <x-responsive-nav-link
+                                :href="route('home')"
+                                :active="request()->routeIs('home')"
+                                wire:navigate>
+                                Home
+                            </x-responsive-nav-link>
                         </li>
                         @role('employee')
                         <li>
-                            <a href="{{route('job-application.index')}}"
-                               class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                            <x-responsive-nav-link
+                                :href="route('job-application.index')"
+                                :active="request()->routeIs('job-application.index')"
+                                wire:navigate>
                                 Application
-                            </a>
+                            </x-responsive-nav-link>
                         </li>
                         <li>
-                            <a href="#"
-                               class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                            <x-responsive-nav-link
+                                :href="route('interview.index')"
+                                :active="request()->routeIs('interview.index')"
+                                wire:navigate>
                                 Interview
-                            </a>
+                            </x-responsive-nav-link>
                         </li>
                         @else
                             <li>
@@ -95,10 +102,10 @@
                                    wire:navigate
                                    class="flex py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
                                     Dashboard
-                                    <x-heroicons::outline.arrow-top-right-on-square class="ml-2"/>
+                                   <x-heroicons::outline.arrow-top-right-on-square class="ml-2"/>
                                 </a>
                             </li>
-                        @endrole
+                            @endrole
 
 
                     </ul>
