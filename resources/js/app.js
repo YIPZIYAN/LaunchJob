5e69bd7e2f13e07670e3649c16ce798bd516b6a9
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-09-18 17:51:41 +0800

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,4 @@
 import './bootstrap';
 import 'flowbite';
-
-import Alpine from 'alpinejs';
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid';
 
-window.Alpine = Alpine;
-
-Alpine.start();
