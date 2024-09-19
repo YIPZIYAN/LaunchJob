e4a3a17906fd61e31b8c6f2a06d909903efa82f2
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-09-20 03:57:11 +0800

    Update profile-edit-form.blade.php

--- resources/views/livewire/auth/profile-edit-form.blade.php
+++ resources/views/livewire/auth/profile-edit-form.blade.php
@@ -22,16 +22,20 @@
                     <form class="mt-6 space-y-6" wire:submit.prevent="submit">
 
                         <div>
-                            <x-input-label for="avatar" value="Avatar" />
-                            <x-wireui-avatar sm
-                                             icon="user"
-                                             :src="Auth::user()->avatar == null ? '': asset('storage/'.Auth::user()->avatar)"/>
-                            <input
-                                wire:model="avatar"
-                                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                                id="file_input" type="file">
-                            @error('avatar') <div class="text-sm text-red-500">{{ $message }}</div> @enderror
-
+                            <x-input-label for="avatar" value="Avatar"/>
+                            <div class="flex space-x-2 mt-2">
+                                <x-wireui-avatar lg
+                                                 icon="user"
+                                                 :src="Auth::user()->avatar == null ? '': asset('storage/'.Auth::user()->avatar)"/>
+                                <div>
+                                    <input
+                                        wire:model="avatar"
+                                        class="block m-1 w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                        id="file_input" type="file">
+                                    @error('avatar')
+                                    <div class="text-sm text-red-500">{{ $message }}</div> @enderror
+                                </div>
+                            </div>
                         </div>
                         <div>
                             <x-wireui-input
