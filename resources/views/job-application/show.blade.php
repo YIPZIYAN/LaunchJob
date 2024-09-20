ea4be17617329afb8eb116f8c02745e1b5d80641
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-09-20 20:45:50 +0800

    temp

--- resources/views/job-application/show.blade.php
+++ resources/views/job-application/show.blade.php
@@ -1,6 +1,7 @@
 <x-guest-layout>
     <div class="p-8 space-y-8">
         <livewire:job-state.employee-select-button :job-application="$jobApplication" />
+        {{Http::room()->get('/')}}
         @include('job-application.partials.show-job-details')
         @include('job-application.partials.show-company-details')
         @if($jobApplication->interviews->isNotEmpty())
